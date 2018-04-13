0374195e29da422f727fbadd04d50392915539ef
Valentin Bourdaud <bourdaudv@gmail.com>
2018-04-13 14:19:03 +0200

    Affichage de l'ArrayList avec boucle for

--- src/User.java
+++ src/User.java
@@ -1,26 +1,52 @@
 import java.util.ArrayList;
 
 public class User {
-	
-	//Declaration des attributs 
-	
+
+	// Declaration des attributs
+
 	String name;
 	Integer age;
 	boolean isMale;
-	
-	//Constructeur 
-	
-	public User() {
-		
+	String sex;
+
+	// Constructeur
+
+	public User(String name, Integer age, boolean isMale) {
+		this.name = name;
+		this.age = age;
+		this.isMale= isMale;
 	}
-	
-	
+
+	public String toString() {
+		if (this.isMale == true)
+			this.sex = "homme";
+
+		else
+			this.sex = "femme";
+		return "Je m'appelle " + this.name + ", j'ai " + this.age + " ans et je suis une " + this.sex;
+	}
+
 	public static void main(String[] args) {
-		ArrayList<User>users = new ArrayList();
-		
-		User u = new User();
+		ArrayList<User> users = new ArrayList();
+
+		User u = new User("Robert", 17, true);
 		users.add(u);
-		
+		User u1 = new User("Stephane", 43, true);
+		users.add(u1);
+		User u2 = new User("Delphine", 23, false);
+		users.add(u2);
+		User u3 = new User("Farid", 27, true);
+		users.add(u3);
+		User u4 = new User("Helene", 22, false);
+		users.add(u4);
+		User u5 = new User("Apolline", 85, false);
+		users.add(u5);
+		User u6 = new User("Sylvie", 14, false);
+		users.add(u6);
+
+		for (int i = 0; i < users.size(); i++) {
+			System.out.println(users.get(i));
+		}
 	}
 
 }
