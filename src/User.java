5509329ac275a1c1729500b0c188ffa456601351
Valentin Bourdaud <bourdaudv@gmail.com>
2018-04-13 14:40:01 +0200

    Rangement par ordre alphabetique

--- src/User.java
+++ src/User.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class User {
 
@@ -14,7 +17,7 @@ public class User {
 	public User(String name, Integer age, boolean isMale) {
 		this.name = name;
 		this.age = age;
-		this.isMale= isMale;
+		this.isMale = isMale;
 	}
 
 	public String toString() {
@@ -43,6 +46,15 @@ public class User {
 		users.add(u5);
 		User u6 = new User("Sylvie", 14, false);
 		users.add(u6);
+
+		Collections.sort(users, new Comparator<User>() {
+
+			public int compare(User u1, User u2) {
+
+				return u1.name.compareTo(u2.name);
+			}
+
+		});
 
 		for (int i = 0; i < users.size(); i++) {
 			System.out.println(users.get(i));
